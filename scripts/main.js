013ad33db1a7ff009ed88ee186b135d0f19ac2d4
nyarbor1 <113961150+nyarbor1@users.noreply.github.com>
2023-07-02 00:35:12 -0400

    stylistic changes and drag/drop functionality

--- scripts/main.js
+++ scripts/main.js
@@ -41,6 +41,7 @@ const createGodElement = (godName, godType) => {
     newGodDiv.setAttribute("data-name", godName);
     newGodDiv.setAttribute("class", "god");
     newGodDiv.setAttribute("data-type", godType);
+    newGodDiv.setAttribute("draggable", "true");
     
     const newGodName = document.createElement("p");
     const nameNode = document.createTextNode(godName)
@@ -53,9 +54,15 @@ const createGodElement = (godName, godType) => {
     newGodDiv.appendChild(newGodName);
     newGodDiv.appendChild(newGodImg);
 
-    newGodDiv.onclick = (e) => {
-        pickGod(godName);
-    }
+    newGodDiv.addEventListener("dragstart", () => {
+        newGodDiv.classList.add("dragging");
+    })
+
+    newGodDiv.addEventListener("dragend", () => {
+        newGodDiv.classList.remove("dragging");
+        // newGodDiv.parentNode.innerHTML = "";
+    })
+
     const picks = document.getElementById("picks");
     picks.appendChild(newGodDiv);
 }
@@ -66,13 +73,37 @@ const emptyGods = () => {
 }
 
 const pickGod = (name) => {
-    alert("pickGod")
-
     const row1Round1Pick1 = document.getElementsByClassName("pick");
     console.log(row1Round1Pick1[0]);
     row1Round1Pick1[0].innerHTML = name;
-    
 }
+
+const banContainers = document.getElementsByClassName("pick");
+for(let i = 0; i < banContainers.length; i++) {
+    banContainers[i].addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+
+    banContainers[i].addEventListener("drop", () => {
+        console.log("dropped")
+        const draggable = document.querySelector('.dragging');
+        banContainers[i].appendChild(draggable);
+    });
+}
+
+const pickContainers = document.getElementsByClassName("player");
+for(let i = 0; i < pickContainers.length; i++) {
+    pickContainers[i].addEventListener("dragover", (event) => {
+       event.preventDefault();
+    });
+
+    pickContainers[i].addEventListener("drop", () => {
+        console.log("dropped")
+        const draggable = document.querySelector('.dragging');
+        pickContainers[i].appendChild(draggable);
+    });
+}
+
 
 
 // const hirez = require("hirez-api");
